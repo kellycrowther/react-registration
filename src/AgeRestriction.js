1e9e0c15e2f562469cf62ee4e5c4086fe0e6bea9
Kelly Crowther <crowther.kelly1@gmail.com>
2018-03-17 14:31:25 -0700

    prop-types added

--- src/AgeRestriction.js
+++ src/AgeRestriction.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+const AgeRestriction = (props) => {
+    return (
+        <div>
+            <select
+                onChange={props.onChange}
+                placeholder="Age Restriction"
+            >
+                {props.restrictions.map((ageLimit, index) => {
+                    return (
+                        <option
+                            key={index}
+                            name={ageLimit}
+                            value={ageLimit}
+                        >
+                            {ageLimit}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+};
 
-const AgeRestriction = (props) => (
-    
-    <div>
-        <select
-            onChange={props.onChange}
-            placeholder="Age Restriction"
-        >
-            {props.restrictions.map((ageLimit, index) => {
-                return (
-                    <option
-                        key={index}
-                        name={ageLimit}
-                        value={ageLimit}
-                    >
-                        {ageLimit}
-                    </option>
-                );
-            })}
-        </select>
-    </div>
-);
+AgeRestriction.propTypes = {
+    restrictions: PropTypes.arrayOf(PropTypes.string).isRequired
+};
 
 export default AgeRestriction;
--- src/Location.js
+++ src/Location.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+const Location = (props) => {
+    return (
+        <div>
+            <select
+                onChange={props.onChange}
+                placeholder="Location"
+            >
+                {props.locations.map((location, index) => {
+                    return (
+                        <option
+                            key={index}
+                            value={location}
+                        >
+                            {location}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+};
 
-const Location = (props) => (
-
-    <div>
-        <select 
-            onChange={props.onChange}
-            placeholder="Location"
-        >
-            {props.locations.map((location, index) => {
-                return (
-                    <option
-                        key = {index}
-                        value={location}
-                    >
-                        {location}
-                    </option>
-                );
-            })}
-        </select>
-    </div>
-);
+Location.propTypes = {
+    locations: PropTypes.arrayOf(PropTypes.string).isRequired
+};
 
 export default Location;
