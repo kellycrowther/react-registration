769526c2c8320c46e80850f7ac59b25af8134170
Kelly Crowther <crowther.kelly1@gmail.com>
2018-03-29 22:03:47 -0700

    added typescript react scripts and working with JSX components

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
   <BrowserRouter>
     <App />
-  </BrowserRouter>), document.getElementById('root'));
+  </BrowserRouter>),
+document.getElementById('root'));
 registerServiceWorker();
