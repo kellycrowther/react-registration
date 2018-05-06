128d1fe3955b6fce5e6f6524a1219360b69bee9f
Kelly Crowther <crowther.kelly1@gmail.com>
2018-05-06 11:22:07 -0700

    submit order hooked up to api

--- src/components/cart/CartSummary.tsx
+++ src/components/cart/CartSummary.tsx
@@ -2,12 +2,45 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Grid, Button, Icon } from 'semantic-ui-react';
 import CartActivity from './CartActivity';
-import Authentication from '../../services/authentication';
 
-const auth = new Authentication();
+const API = 'http://localhost:3111';
+const ORDER_ROUTE = '/order';
 
 export default class CartSummary extends Component<any, any> {
+  getActivityIds = () => {
+    let activityIds: Array<any> = [];
+    let activityIdsObject: Object;
+    for (let x = 0; x < this.props.cartSelection.length; x++) {
+      activityIds.push(this.props.cartSelection[x].activity_id);
+    }
 
+    activityIdsObject = { activityIds: activityIds };
+    return activityIdsObject;
+  }
+
+  submitOrder = () => {
+    let activityIds = this.getActivityIds();
+    console.log('activityIds: ', activityIds);
+    let token = localStorage.getItem('token');
+    console.log('token: ', token);
+    fetch(API + ORDER_ROUTE, {
+      method: 'POST',
+      body: JSON.stringify(activityIds),
+      headers: new Headers({
+        'Authorization': `bearer ${token}`,
+        'Content-Type': 'application/json'
+      })
+    })
+      .then(res => {
+        console.log('res: ', res);
+        res.json().then(payload => {
+          console.log('payload: ', payload);
+        });
+      })
+      .catch(err => {
+        console.log('err: ', err);
+      });
+  }
   render() {
     const cartActivityComponents = this.props.cartSelection.map((activity: any, index: number) => {
       return (
@@ -39,7 +72,7 @@ export default class CartSummary extends Component<any, any> {
           animated='fade'
           inverted
           color='blue'
-          onClick={auth.secret}
+          onClick={this.submitOrder}
         >
           <Button.Content visible>
             Finalize Registeration
